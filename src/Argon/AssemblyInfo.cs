2ebdcf82d6558a06b1203568ce3693bb6a923672
Simon Cropp <simon.cropp@gmail.com>
2022-02-26 11:03:04 +1100

    simplify license

--- src/Argon/AssemblyInfo.cs
+++ src/Argon/AssemblyInfo.cs
@@ -1,27 +1,6 @@
-#region License
-// Copyright (c) 2007 James Newton-King
-//
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-#endregion
+// Copyright (c) 2007 James Newton-King. All rights reserved.
+// Use of this source code is governed by The MIT License,
+// as found in the license.md file.
 
 [assembly: InternalsVisibleTo("Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a93b41fc36cf1f276ace94f8afdcaadacf2244178414806e27da3511599a51f2bd7d737583b0331d2f38aec95c022f8c6eca7a2f87c797ecad72da798637f8591f1268db94fb6db989194b8e6d9a492e2ef544e6c6e0de800696572f195f0ed73f0acd1b9dabd62fd5b1b40c54eccddfa8003813089b85b5c721f7c4d297d5ca")]
 [assembly: InternalsVisibleTo("Argon.Xml, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a93b41fc36cf1f276ace94f8afdcaadacf2244178414806e27da3511599a51f2bd7d737583b0331d2f38aec95c022f8c6eca7a2f87c797ecad72da798637f8591f1268db94fb6db989194b8e6d9a492e2ef544e6c6e0de800696572f195f0ed73f0acd1b9dabd62fd5b1b40c54eccddfa8003813089b85b5c721f7c4d297d5ca")]
