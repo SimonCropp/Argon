e8966f685413ba0394fa52fbc8a830fce1083d4b
Simon Cropp <simon.cropp@gmail.com>
2022-05-30 12:06:37 +1000

    docs

--- src/Tests/SystemJsonTests.cs
+++ src/Tests/SystemJsonTests.cs
@@ -1,8 +1,63 @@
-public class SystemJsonTests
+#if NET6_0
+
+public class DocsTests
 {
     [Fact]
     public void Foo()
     {
-        // System.Text.Json.JsonSerializer.Deserialize<int?>("");
+        var solutionDirectory = AttributeReader.GetSolutionDirectory();
+        var docsDirectory = Path.Combine(solutionDirectory, "../docs");
+        docsDirectory = Path.GetFullPath(docsDirectory);
+        var includeFile = Path.Combine(docsDirectory, "index.include.md");
+        var samplesDirectory = Path.Combine(docsDirectory, "Samples");
+        File.Delete(includeFile);
+
+        var builder = new StringBuilder();
+        var level = 0;
+        AddFiles(builder, docsDirectory, docsDirectory, "");
+        AddDirectory(ref level, builder, samplesDirectory, docsDirectory);
+
+        File.WriteAllText(includeFile, builder.ToString());
+    }
+
+    static void AddDirectory(ref int level, StringBuilder builder, string directory, string docsDirectory)
+    {
+        level++;
+
+        var directoryIndent = new string(' ', level);
+        var url = GetUrl(docsDirectory, directory);
+        builder.AppendLine($"{directoryIndent} * [{Path.GetFileName(directory)}]({url})");
+        foreach (var nestedDirectory in Directory.EnumerateDirectories(directory))
+        {
+            AddDirectory(ref level, builder, nestedDirectory, docsDirectory);
+        }
+
+        AddFiles(builder, directory, docsDirectory, directoryIndent);
+
+        level--;
+    }
+
+    static void AddFiles(StringBuilder builder, string directory, string docsDirectory, string directoryIndent)
+    {
+        foreach (var file in Directory.EnumerateFiles(directory, "*.md"))
+        {
+            AddFile(builder, docsDirectory, file, directoryIndent);
+        }
+    }
+
+    static void AddFile(StringBuilder builder, string docsDirectory, string file, string directoryIndent)
+    {
+        var url = GetUrl(docsDirectory, file);
+        builder.AppendLine($"{directoryIndent}  * [{Path.GetFileNameWithoutExtension(file)}]({url})");
+    }
+
+    static string GetUrl(string docsDirectory, string file)
+    {
+        var fullPath = Path.GetFullPath(file);
+        var suffix = fullPath.Replace(docsDirectory, "")
+            .Replace('\\', '/');
+        return $"/docs{suffix}";
     }
 }
+
+#endif
