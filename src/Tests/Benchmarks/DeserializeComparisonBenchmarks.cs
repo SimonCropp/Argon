fc700274ee8cfc6cfccc5467fc00c37d0148de57
Simon Cropp <simon.cropp@gmail.com>
2022-08-03 19:35:51 +1000

    redundant constructor

--- src/Tests/Benchmarks/DeserializeComparisonBenchmarks.cs
+++ src/Tests/Benchmarks/DeserializeComparisonBenchmarks.cs
@@ -2,7 +2,6 @@
 // Use of this source code is governed by The MIT License,
 // as found in the license.md file.
 
-
 using System.Runtime.Serialization.Json;
 using BenchmarkDotNet.Attributes;
 using TestObjects;
