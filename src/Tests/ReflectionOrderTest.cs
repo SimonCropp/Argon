cc584017d106bd9a83e77920d2d63893e8b31c6d
Simon <simon.cropp@gmail.com>
2022-11-01 13:31:46 +1100

    Create ReflectionOrderTest.cs

---
+++ src/Tests/ReflectionOrderTest.cs
@@ -0,0 +1,20 @@
+﻿public class ReflectionOrderTest : TestFixtureBase
+{
+    [Fact]
+    public void Test()
+    {
+        typeof(Target).GetProperty("Member2");
+        var result = JsonConvert.SerializeObject(new Target(), Formatting.Indented);
+        Assert.Equal(@"{
+  ""Member1"": 0,
+  ""Member2"": 0
+}", result);
+    }
+
+    class Target
+    {
+        public int Member1 { get; set; }
+        public int Member2 { get; set; }
+    }
+
+}
