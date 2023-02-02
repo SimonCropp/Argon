280e739ccbe7b4f1217c500cf5c9fd4e34f57074
Simon Cropp <simon.cropp@gmail.com>
2023-02-03 10:40:44 +1100

    expose GetDictionaryPropertyName as virtual

--- src/Argon/Serialization/JsonSerializerInternalWriter.cs
+++ src/Argon/Serialization/JsonSerializerInternalWriter.cs
@@ -1021,7 +1021,7 @@ class JsonSerializerInternalWriter : JsonSerializerInternalBase
         }
     }
 
-    static string GetDictionaryPropertyName(object key, JsonContract keyContract, Func<string, string>? keyResolver, out bool escape)
+    protected virtual string GetDictionaryPropertyName(object key, JsonContract keyContract, Func<string, string>? keyResolver, out bool escape)
     {
         var propertyName = GetDictionaryPropertyName(key, keyContract, out escape);
 
@@ -1033,7 +1033,7 @@ class JsonSerializerInternalWriter : JsonSerializerInternalBase
         return keyResolver(propertyName);
     }
 
-    static string GetDictionaryPropertyName(object key, JsonContract contract, out bool escape)
+    protected virtual string GetDictionaryPropertyName(object key, JsonContract contract, out bool escape)
     {
         if (contract.ContractType == JsonContractType.Primitive)
         {
