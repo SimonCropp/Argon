26a2ffa0f26861f65f062404ed49f3c14d63e291
Simon Cropp <simon.cropp@gmail.com>
2023-02-03 12:03:35 +1100

    Update DictionaryKeyContractResolver.cs

--- src/Tests/TestObjects/DictionaryKeyContractResolver.cs
+++ src/Tests/TestObjects/DictionaryKeyContractResolver.cs
@@ -12,7 +12,7 @@ public class DictionaryKeyContractResolver : DefaultContractResolver
     protected override string ResolvePropertyName(string propertyName)
     {
 #if NET5_0_OR_GREATER
-            return propertyName.ToUpperInvariant();
+        return propertyName.ToUpperInvariant();
 #else
         return propertyName.ToUpper(InvariantCulture);
 #endif
