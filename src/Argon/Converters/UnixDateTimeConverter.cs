cc5d9dbd393c8a12ded34fa392713b903d1247e4
Simon Cropp <simon.cropp@gmail.com>
2023-02-11 21:50:22 +1100

    Update UnixDateTimeConverter.cs

--- src/Argon/Converters/UnixDateTimeConverter.cs
+++ src/Argon/Converters/UnixDateTimeConverter.cs
@@ -36,11 +36,9 @@ public class UnixDateTimeConverter : DateTimeConverterBase
     /// <c>false</c> to throw an exception when a date being converted to or from JSON
     /// occurred before Unix epoch. The default value is <c>false</c>.
     /// </param>
-    public UnixDateTimeConverter(bool allowPreEpoch)
-    {
+    public UnixDateTimeConverter(bool allowPreEpoch) =>
         AllowPreEpoch = allowPreEpoch;
-    }
-    
+
     /// <summary>
     /// Writes the JSON representation of the object.
     /// </summary>
