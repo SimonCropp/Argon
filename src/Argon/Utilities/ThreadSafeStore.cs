0a7bec83d1a05221b0020b487c4b9f10556fcfd5
Simon Cropp <simon.cropp@gmail.com>
2023-09-07 20:34:43 +1000

    Revert "Update ThreadSafeStore.cs"
    This reverts commit 417eff82bd896b9884f15fba9ba6f097770f6914.

--- src/Argon/Utilities/ThreadSafeStore.cs
+++ src/Argon/Utilities/ThreadSafeStore.cs
@@ -2,10 +2,16 @@
 // Use of this source code is governed by The MIT License,
 // as found in the license.md file.
 
-class ThreadSafeStore<TKey, TValue>(Func<TKey, TValue> creator)
-    where TKey : notnull
+class ThreadSafeStore<TKey, TValue> where TKey : notnull
 {
-    ConcurrentDictionary<TKey, TValue> concurrentStore = new();
+    readonly ConcurrentDictionary<TKey, TValue> concurrentStore;
+    readonly Func<TKey, TValue> creator;
+
+    public ThreadSafeStore(Func<TKey, TValue> creator)
+    {
+        this.creator = creator;
+        concurrentStore = new();
+    }
 
     public TValue Get(TKey key) =>
         concurrentStore.GetOrAdd(key, creator);
