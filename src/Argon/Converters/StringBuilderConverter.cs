599b443feca4694dd00cb9401d76ad08f9be56ac
Simon Cropp <simon.cropp@gmail.com>
2023-09-08 20:36:45 +1000

    more converters (#245)

---
+++ src/Argon/Converters/StringBuilderConverter.cs
@@ -0,0 +1,18 @@
+namespace Argon;
+
+public class StringBuilderConverter :
+    JsonConverter<StringBuilder>
+{
+    public override void WriteJson(JsonWriter writer, StringBuilder? value, JsonSerializer serializer) =>
+        writer.WriteValue(value?.ToString());
+
+    public override StringBuilder? ReadJson(JsonReader reader, Type type, StringBuilder? existingValue, bool hasExisting, JsonSerializer serializer)
+    {
+        if (reader.Value is string value)
+        {
+            return new(value);
+        }
+
+        return null;
+    }
+}
---
+++ src/Argon/Converters/StringWriterConverter.cs
@@ -0,0 +1,18 @@
+namespace Argon;
+
+public class StringWriterConverter :
+    JsonConverter<StringWriter>
+{
+    public override void WriteJson(JsonWriter writer, StringWriter? value, JsonSerializer serializer) =>
+        writer.WriteValue(value?.ToString());
+
+    public override StringWriter? ReadJson(JsonReader reader, Type type, StringWriter? existingValue, bool hasExisting, JsonSerializer serializer)
+    {
+        if (reader.Value is string value)
+        {
+            return new(new StringBuilder(value));
+        }
+
+        return null;
+    }
+}
