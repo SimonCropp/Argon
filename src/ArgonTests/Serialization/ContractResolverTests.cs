8e548c74c625aeb2105411720b236be33da2dadf
Simon Cropp <simon.cropp@gmail.com>
2023-09-17 22:47:10 +1000

    Update ContractResolverTests.cs

--- src/ArgonTests/Serialization/ContractResolverTests.cs
+++ src/ArgonTests/Serialization/ContractResolverTests.cs
@@ -6,13 +6,8 @@ using System.Text.RegularExpressions;
 using TestObjects;
 // ReSharper disable PossibleMultipleEnumeration
 
-public class DynamicContractResolver : DefaultContractResolver
+public class DynamicContractResolver(char startingWithChar) : DefaultContractResolver
 {
-    readonly char startingWithChar;
-
-    public DynamicContractResolver(char startingWithChar) =>
-        this.startingWithChar = startingWithChar;
-
     protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
     {
         var properties = base.CreateProperties(type, memberSerialization);
