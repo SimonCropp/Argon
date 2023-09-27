9213945b48ac83032069c3b63726a53750825751
Simon Cropp <simon.cropp@gmail.com>
2023-09-27 19:34:49 +1000

    .

---
+++ src/Argon/Caching/AttributeCache.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2007 James Newton-King. All rights reserved.
+// Use of this source code is governed by The MIT License,
+// as found in the license.md file.
+
+static class AttributeCache<T> where T : Attribute
+{
+    static ThreadSafeStore<ICustomAttributeProvider, T?> cache = new(JsonTypeReflector.GetAttribute<T>);
+
+    public static T? GetAttribute(ICustomAttributeProvider provider) =>
+        cache.Get(provider);
+}
--- src/Argon/Caching/TypeAttributeCache.cs
+++ src/Argon/Caching/TypeAttributeCache.cs
@@ -1,16 +1,4 @@
-﻿// Copyright (c) 2007 James Newton-King. All rights reserved.
-// Use of this source code is governed by The MIT License,
-// as found in the license.md file.
-
-static class AttributeCache<T> where T : Attribute
-{
-    static ThreadSafeStore<ICustomAttributeProvider, T?> cache = new(JsonTypeReflector.GetAttribute<T>);
-
-    public static T? GetAttribute(ICustomAttributeProvider provider) =>
-        cache.Get(provider);
-}
-
-static class TypeAttributeCache
+﻿static class TypeAttributeCache
 {
     public class Info
     {
