0178d1330c902599332192ee3cfce62b7680882b
Simon Cropp <simon.cropp@gmail.com>
2024-02-10 23:09:46 +1100

    .

--- src/Argon/Converters/StringBuilderConverter.cs
+++ src/Argon/Converters/StringBuilderConverter.cs
@@ -3,7 +3,7 @@ namespace Argon;
 public class StringBuilderConverter :
     JsonConverter<StringBuilder>
 {
-    public override void WriteJson(JsonWriter writer, StringBuilder? value, JsonSerializer serializer) =>
+    public override void WriteJson(JsonWriter writer, StringBuilder value, JsonSerializer serializer) =>
         writer.WriteValue(value);
 
     public override StringBuilder? ReadJson(JsonReader reader, Type type, StringBuilder? existingValue, bool hasExisting, JsonSerializer serializer)
--- src/Argon/Converters/StringWriterConverter.cs
+++ src/Argon/Converters/StringWriterConverter.cs
@@ -3,8 +3,8 @@ namespace Argon;
 public class StringWriterConverter :
     JsonConverter<StringWriter>
 {
-    public override void WriteJson(JsonWriter writer, StringWriter? value, JsonSerializer serializer) =>
-        writer.WriteValue(value?.GetStringBuilder());
+    public override void WriteJson(JsonWriter writer, StringWriter value, JsonSerializer serializer) =>
+        writer.WriteValue(value.GetStringBuilder());
 
     public override StringWriter? ReadJson(JsonReader reader, Type type, StringWriter? existingValue, bool hasExisting, JsonSerializer serializer)
     {
