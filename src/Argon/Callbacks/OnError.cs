f5f18f63e25cab068ea29439c57423770da6a3fe
Simon Cropp <simon.cropp@gmail.com>
2024-07-30 19:17:46 +1000

    Delete OnError.cs

--- src/Argon/Callbacks/OnError.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Argon;
-
-/// <summary>
-/// Handles <see cref="JsonSerializer" /> serialization error callback events.
-/// </summary>
-public delegate void OnError(object? currentObject, object? originalObject, ErrorLocation location, Exception exception, Action markAsHandled);
