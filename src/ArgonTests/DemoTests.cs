5f881841dbb7dfbe8595841d5ac358cf9fe71c29
Simon Cropp <simon.cropp@gmail.com>
2024-10-24 20:53:10 +1100

    Update DemoTests.cs

--- src/ArgonTests/DemoTests.cs
+++ src/ArgonTests/DemoTests.cs
@@ -144,10 +144,10 @@ public class DemoTests : TestFixtureBase
         var dailyRegistrations = new Dictionary<DateTime, int>
         {
             {
-                new DateTime(2014, 6, 1), 23
+                new(2014, 6, 1), 23
             },
             {
-                new DateTime(2014, 6, 2), 50
+                new(2014, 6, 2), 50
             }
         };
 
